f303a087c97fc07337219ff833658e56862801de
jarichooo <barjajoshuajericho@gmail.com>
2025-06-07 18:21:28 +0800

    New setup

--- SariScan.API/Models/DTOS/CheckoutDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace SariScan.API.Models.DTOS
-{
-    public class CheckoutDTO
-    {
-        public required string barcode { get; set; }
-        public required int quantity { get; set; }
-    }
-}
---
+++ main.py
@@ -0,0 +1,4 @@
+import mysql.connector # communicates 
+from mysql.connector import Error
+from dotenv import load_dotenv # loads variables from .env 
+import os # reads environment variables from .env
